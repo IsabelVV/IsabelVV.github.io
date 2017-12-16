881fb63ff6300803795d86ba6904d0a80546e06d
IsabelVV <33789537+IsabelVV@users.noreply.github.com>
2017-12-16 10:47:05 -0800

    Update scripts1.js

--- scripts1.js
+++ scripts1.js
@@ -1,1 +1,6 @@
-
+function generate() {
+var project = document.getElementById('project');
+var projects=["5 second rule", "caffeine's effects on plants", "dichotomous key"];
+var random=Math.floor(Math.random()*3);
+  project.innerHTML=projects[random];
+}
