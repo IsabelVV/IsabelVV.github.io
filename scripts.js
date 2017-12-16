dcfe1e72c5c45d02dbc5fadab23297706c64eafe
IsabelVV <33789537+IsabelVV@users.noreply.github.com>
2017-12-16 10:40:36 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 function generate() {
 project = document.getElementById('project');
-projects=["Are Childproof Containers Really Childproof?", "Is Smiling Contagious?"];
-random=Math.random();
+projects=["Are Childproof Containers Really Childproof?", "Is Smiling Contagious?", "Does temperature increase with stress?"];
+random=Math.floor(Math.random()*3);
+  project.innerHTML=projects[random];
+}
